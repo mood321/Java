9534b59efb135b04da8a47ffa1ee26c70d7e6ce9
mood321 <371428187@qq.com>
2019-04-25 09:20:27 +0800

    排序算法 二分

--- src/main/java/Conllection/SelectSort.java
+++ src/main/java/Conllection/SelectSort.java
@@ -9,13 +9,55 @@ public class SelectSort {
         int b[] = {5, 2, 8, 4, 1, 9, 16};
           System.out.println("选择排序法"+ Arrays.toString(select(b)));
         int c[] = {5, 2, 8, 4, 1, 9, 16};
-        //   System.out.println("插入排序法"+Arrays.toString(insert(c)));
+        System.out.println("插入排序法"+Arrays.toString(insert(c)));
 
-        //    System.out.println("二分 位置:"+find(a,8));
+            System.out.println("二分 位置:"+find(bubble(a),8));
 
 
     }
 
+    /**
+     * 二分查找
+     * @param a
+     * @param i
+     * @return
+     */
+    private static int find(int[] a, int i) {
+
+        int beg=0,end=a.length;
+        while(beg<=end){
+            int mid=beg+(end-beg)/2;
+            if(a[mid]==i){
+                return mid;
+            }else if(i<a[mid]){
+                end=mid-1;
+            }else{
+                beg=mid+1;
+            }
+        }
+        return a.length;
+    }
+
+    private static int[] insert(int[] c) {
+        int work;
+        for(int i=1;i<c.length;i++){
+            int tem=c[i];
+            work=i;
+            while(work>0&&c[work-1]<tem){
+                c[work]=c[work-1];
+                work--;
+            }
+
+            c[work]=tem;
+        }
+        return c;
+    }
+
+    /**
+     * 选择排序法
+     * @param b
+     * @return
+     */
     private static int[] select(int[] b) {
 
         for (int i=0;i<b.length-1;i++){
