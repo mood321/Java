75ce308e4493e0dfdc6d46fb9f64ef3845c16f60
mood321 <371428187@qq.com>
2019-06-20 09:24:14 +0800

    更新 枚举demo

--- src/main/java/clazz/EnumDemo.java
+++ src/main/java/clazz/EnumDemo.java
@@ -1,4 +1,22 @@
 package clazz;
 
-public class EnumDemo {
+public enum EnumDemo {
+    DEMO1("1") {
+        @Override
+        void run() {
+            System.out.println("zheshi 1");
+        }
+    };
+    private String code;
+
+    abstract void run();
+
+    EnumDemo(String code) {
+        this.code = code;
+    }
+
+    public static void main(String[] args) {
+        EnumDemo demo = EnumDemo.valueOf("DEMO1");
+        demo.run();
+    }
 }
