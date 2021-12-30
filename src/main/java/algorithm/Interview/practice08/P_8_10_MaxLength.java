69f740a5762006b681f60174d790fe040f1f1a59
mood321 <hejiab@foxmail.com>
2021-12-31 00:05:50 +0800

    算法-未排序数组和为定值的最长子数组长度 (负数 正数 0)

---
+++ src/main/java/algorithm/Interview/practice08/P_8_10_MaxLength.java
@@ -0,0 +1,35 @@
+package algorithm.Interview.practice08;
+
+import scala.Int;
+
+import java.util.HashMap;
+
+/**
+ * <p></> 未排序数组和为定值的最长子数组长度
+ *   子数组必定是连续的
+ * @author mood321
+ */
+public class P_8_10_MaxLength {
+    public int getManLenght(int[] arr, int k) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+
+        }
+        int len = 0;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum =0;
+        map.put(sum,-1);
+        for (int i = 0; i < arr.length; i++) {
+            sum+= arr[i];
+            if(map.containsKey(sum- k)){
+                len = Math.max(len,i-map.get(sum-k));
+
+            }
+            if(!map.containsKey(sum)){
+                map.put(sum,i);
+            }
+        }
+        return len;
+    }
+}
